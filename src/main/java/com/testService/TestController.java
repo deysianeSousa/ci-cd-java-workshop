c47e41a80d4071bf514dd93ca89e5fbc5338a857
deysiane.de.sousa <Sophia_1429>
2019-08-03 15:48:40 -0300

    Remover erros Lint

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
 @CrossOrigin
 public class TestController {
 
-	@GetMapping("/home")
+    /**
+     *
+     * @return teste
+     */
+    @GetMapping("/home")
 	public String homeRequest() {
-		return "Hello World!";
+	    return "Hello World!";
 	}
 }
